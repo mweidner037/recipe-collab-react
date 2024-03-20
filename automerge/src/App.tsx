29d9991a0549dddba4468521910f6af1180437f5
Matthew Weidner <17693586+mweidner037@users.noreply.github.com>
2024-03-20 16:09:02 -0400

    automerge schema draft

--- automerge/src/App.tsx
+++ automerge/src/App.tsx
@@ -1,15 +1,54 @@
-import automergeLogo from '/automerge.png'
-import './App.css'
-import { default as A } from "@automerge/automerge/next"
-import { AutomergeUrl } from '@automerge/automerge-repo'
-import { useDocument } from '@automerge/automerge-repo-react-hooks'
+import { AutomergeUrl } from "@automerge/automerge-repo";
+import { useDocument } from "@automerge/automerge-repo-react-hooks";
+import "./App.css";
+import automergeLogo from "/automerge.png";
 
-interface CounterDoc {
-  counter: A.Counter
+type Unit = "kg" | "g" | "mg" | "L" | "mL" | "ct";
+const AllUnits: Unit[] = ["kg", "g", "mg", "L", "mL", "ct"];
+export const defaultUnit: Unit = "g";
+
+export interface Ingredient {
+  /**
+   * To make edits win over concurrent deletes, we set this to true each
+   * time the ingredient is edited, and treat it as true if any of
+   * the multi-values are true (ignoring the default LWW value).
+   */
+  present: boolean;
+  /**
+   * Hack: To avoid figuring out an Automerge list-with-move, we just
+   * assign each ingredient a position from the position-strings library
+   * and sort by those (lexicographically).
+   */
+  position: string;
+  /**
+   * LWW semantics.
+   * TODO: change to text CRDT semantics?
+   */
+  text: string;
+  amountUnscaled: number;
+  units: Unit;
+}
+
+export interface RecipeDoc {
+  /**
+   * LWW semantics.
+   */
+  recipeName: string;
+  scale: number;
+  /**
+   * Array order is ignored - use ingr.position instead.
+   *
+   * Also, set ingr.present to false instead of deleting.
+   */
+  ingredients: Ingredient[];
+  /**
+   * Rich-text semantics.
+   */
+  instructions: string;
 }
 
 function App({ docUrl }: { docUrl: AutomergeUrl }) {
-  const [doc, changeDoc] = useDocument<CounterDoc>(docUrl)
+  const [doc, changeDoc] = useDocument<RecipeDoc>(docUrl);
 
   return (
     <>
@@ -23,15 +62,13 @@ function App({ docUrl }: { docUrl: AutomergeUrl }) {
         <button onClick={() => changeDoc((d) => d.counter.increment(1))}>
           count is {doc && doc.counter.value}
         </button>
-        <p>
-          Open this page in another tab to watch the updates synchronize
-        </p>
+        <p>Open this page in another tab to watch the updates synchronize</p>
       </div>
       <p className="read-the-docs">
         Built with Automerge, Vite, React, and TypeScript
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- automerge/src/main.tsx
+++ automerge/src/main.tsx
@@ -1,40 +1,59 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import * as A from "@automerge/automerge"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App, { RecipeDoc, defaultUnit } from "./App.tsx";
 
-import './index.css'
-import { isValidAutomergeUrl, Repo, DocHandle } from '@automerge/automerge-repo'
-import { BrowserWebSocketClientAdapter } from '@automerge/automerge-repo-network-websocket'
-import { IndexedDBStorageAdapter } from "@automerge/automerge-repo-storage-indexeddb"
-import { RepoContext } from '@automerge/automerge-repo-react-hooks'
+import {
+  DocHandle,
+  Repo,
+  isValidAutomergeUrl,
+} from "@automerge/automerge-repo";
+import { BrowserWebSocketClientAdapter } from "@automerge/automerge-repo-network-websocket";
+import { RepoContext } from "@automerge/automerge-repo-react-hooks";
+import { IndexedDBStorageAdapter } from "@automerge/automerge-repo-storage-indexeddb";
+import { PositionSource } from "position-strings";
+import "./index.css";
 
 const repo = new Repo({
   network: [new BrowserWebSocketClientAdapter("wss://sync.automerge.org")],
   storage: new IndexedDBStorageAdapter(),
-})
+});
 
 declare global {
   interface Window {
-    handle: DocHandle<unknown>
+    handle: DocHandle<unknown>;
   }
 }
 
-const rootDocUrl = `${document.location.hash.substring(1)}`
-let handle
+const rootDocUrl = `${document.location.hash.substring(1)}`;
+let handle;
 if (isValidAutomergeUrl(rootDocUrl)) {
-  handle = repo.find(rootDocUrl)
+  handle = repo.find(rootDocUrl);
 } else {
-  handle = repo.create<{ counter?: A.Counter }>()
-  handle.change(d => d.counter = new A.Counter())
+  handle = repo.create<RecipeDoc>({
+    recipeName: "Untitled",
+    scale: 0,
+    ingredients: [
+      // Start with an empty ingredient, for convenience.
+      {
+        present: true,
+        // Arbitrary valid starting position.
+        position: new PositionSource({ ID: "INIT" }).createBetween(),
+        text: "",
+        amountUnscaled: 1,
+        units: defaultUnit,
+      },
+    ],
+    // Need a starting "\n" to match Quill's starting state.
+    instructions: "\n",
+  });
 }
-const docUrl = document.location.hash = handle.url
-window.handle = handle // we'll use this later for experimentation
+const docUrl = (document.location.hash = handle.url);
+window.handle = handle; // we'll use this later for experimentation
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <RepoContext.Provider value={repo}>
       <App docUrl={docUrl} />
     </RepoContext.Provider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
